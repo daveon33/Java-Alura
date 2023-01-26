9ca66164a4d8f68bfd6e8a104e428d768c46f4a3
David Gonzalez <davidgonzalez5910@gmail.com>
2023-01-26 10:26:34 -0500

    Practicing casting

--- src/test/TestArrayReferencias.java
+++ src/test/TestArrayReferencias.java
@@ -6,7 +6,7 @@ public class TestArrayReferencias {
 	
 	public static void main(String[] args) {
 		
-		CuentaCorriente[] cuentas = new CuentaCorriente[5];
+		Cuenta[] cuentas = new Cuenta[5];
 		
 		CuentaCorriente cc1 = new CuentaCorriente(2, 10);
 		CuentaCorriente cc2 = new CuentaCorriente(3, 11);
@@ -15,5 +15,10 @@ public class TestArrayReferencias {
 		cuentas[1] = cc2;
 		
 		System.out.println(cuentas[0].getNumero());
+		
+		CuentaCorriente ref = (CuentaCorriente) cuentas[0];
+		System.out.println(cc2.getNumero());
+		System.out.println(ref.getNumero());
+		
 	}
 }
